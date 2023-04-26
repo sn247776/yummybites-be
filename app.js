9163dc14c6d3aea0294baf55b96d6e56c0ee8911
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-04-26 21:56:40 +0530

    login created

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ import express from "express";
 import { config } from "dotenv";
 import userRouter from "./routes/user.js";
 import bodyParser from "body-parser";
+import cors from "cors"
 
 export const app = express();
 app.use(bodyParser.json());
 
+// congig.env file must be define 1st on the top to use the env variable. otherwiste face env varable not work.
 config({
     path: "./data/config.env",
   });
+
+app.use(
+  cors({
+    origin: [process.env.FE_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 //Using Routes
 app.use("/api/v1/users", userRouter);
--- routes/user.js
+++ routes/user.js
@@ -5,25 +5,56 @@ import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-
-router.post("/register", 
-body('email').isEmail(),
-body('name').isLength({min: 3}),
-body('location').isLength({min: 5}),
-body('password','Password Length Must be 8 Characters').isLength({min: 8}),
-async (req, res)=>{
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("name").isLength({ min: 3 }),
+  body("location").isLength({ min: 5 }),
+  body("password", "Password Length Must be 8 Characters").isLength({ min: 8 }),
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array() })
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-   try {
-    const { name, email, password, location } = req.body;
-    await User.create({  name, email, password, location });
-    res.json({success:true});
-   } catch (error) {
-    console.log(error)
-    res.json({success:false});
-   }
+    try {
+      const { name, email, password, location } = req.body;
+      await User.create({ name, email, password, location });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+
+router.post('/login', [
+    body('email', "Enter a Valid Email").isEmail(),
+    body('password', "Password cannot be blank").exists(),
+], async (req, res) => {
+    let success = false
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });  //{email:email} === {email}
+        if (!user) {
+            return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
+        }
+
+        if (req.body.password !== user.password) {
+            return res.status(400).json({ success, error: "password not match" });
+        }
+
+        res.json({ success: true })
+
+
+    } catch (error) {
+        console.error(error.message)
+        res.send("Server Error")
+    }
 })
 
 export default router;
