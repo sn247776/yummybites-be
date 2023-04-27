369621c0f4b88fbfc498d5892660af5f16500a29
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-04-27 22:04:41 +0530

    new update

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { User } from "../models/user.js";
-
 import { body, validationResult } from "express-validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -17,44 +18,62 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      const { name, email, password, location } = req.body;
-      await User.create({ name, email, password, location });
+      const { name, email, location, password } = req.body;
+      let userData = await User.findOne({ email });
+      if (userData) {
+        return res.status(400).json({ errors: "userfound" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      userData = await User.create({
+        name,
+        email,
+        location,
+        password: hashedPassword,
+      });
       res.json({ success: true });
     } catch (error) {
-      console.log(error);
       res.json({ success: false });
     }
   }
 );
 
-router.post('/login', [
-    body('email', "Enter a Valid Email").isEmail(),
-    body('password', "Password cannot be blank").exists(),
-], async (req, res) => {
-    let success = false
+router.post(
+  "/login",
+  [
+    body("email", "Enter a Valid Email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() }); 
     }
-
     const { email, password } = req.body;
     try {
-        let user = await User.findOne({ email });  //{email:email} === {email}
-        if (!user) {
-            return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res.json({ success: false, error: "Enter a valid Email" });
+      }
+      const isMatch = await bcrypt.compare(password, userData.password);
+
+      if (!isMatch) {
+        return res.json({ success: false, error: "password not match" });
+      }
+
+      const data = {
+        user:{
+          id:userData.id
         }
-
-        if (req.body.password !== user.password) {
-            return res.status(400).json({ success, error: "password not match" });
-        }
-
-        res.json({ success: true })
-
-
+      }
+      const authToken = jwt.sign(data,process.env.JWT_SECRET)
+      res.json({ success: true, authToken:authToken });
     } catch (error) {
-        console.error(error.message)
-        res.send("Server Error")
+      console.error(error.message);
+      res.send("Server Error");
     }
-})
+  }
+);
 
 export default router;
