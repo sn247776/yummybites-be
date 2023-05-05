202bae4d55b448ff1e07eaf4bf999a49f7168bba
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-05 23:37:04 +0530

    backend updated

--- routes/user.js
+++ routes/user.js
@@ -10,8 +10,8 @@ router.post(
   "/register",
   body("email").isEmail(),
   body("name").isLength({ min: 3 }),
-  body("location").isLength({ min: 5 }),
-  body("password", "Password Length Must be 8 Characters").isLength({ min: 8 }),
+  body("location").isLength({ min: 3 }),
+  body("password", "Password Length Must be 8 Characters").isLength({ min: 3 }),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -21,7 +21,7 @@ router.post(
       const { name, email, location, password } = req.body;
       let userData = await User.findOne({ email });
       if (userData) {
-        return res.status(400).json({ errors: "userfound" });
+        return res.json({ message: "User Allready Exist" });
       }
       const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -31,9 +31,15 @@ router.post(
         location,
         password: hashedPassword,
       });
-      res.json({ success: true });
+      const data = {
+        user:{
+          id:userData.id
+        }
+      }
+      const authToken = jwt.sign(data,process.env.JWT_SECRET)
+      res.json({ success: true, message:"Register Successfully",authToken:authToken });
     } catch (error) {
-      res.json({ success: false });
+      res.json({ success: false , message:"Please Enter Valid Details"});
     }
   }
 );
@@ -45,7 +51,6 @@ router.post(
     body("password", "Password cannot be blank").exists(),
   ],
   async (req, res) => {
-    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() }); 
@@ -54,12 +59,12 @@ router.post(
     try {
       let userData = await User.findOne({ email });
       if (!userData) {
-        return res.json({ success: false, error: "Enter a valid Email" });
+        return res.json({ success: false, message: "Enter a valid Email" });
       }
       const isMatch = await bcrypt.compare(password, userData.password);
 
       if (!isMatch) {
-        return res.json({ success: false, error: "password not match" });
+        return res.json({ success: false, message: "password not match" });
       }
 
       const data = {
@@ -68,7 +73,7 @@ router.post(
         }
       }
       const authToken = jwt.sign(data,process.env.JWT_SECRET)
-      res.json({ success: true,authToken:authToken });
+      res.json({ success: true , authToken:authToken, message: "Login Successfull" });
     } catch (error) {
       console.error(error.message);
       res.send("Server Error");
